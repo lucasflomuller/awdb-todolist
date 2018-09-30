3e5b9301fb541542fbb9ef01844b06a830d767fd
lucasflomuller <lucasflorianomuller@gmail.com>
2018-09-30 19:37:14 +0000

    connect form to api

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,38 @@
 $(document).ready(() => {
     $.getJSON("/api/todos")
     .then(addTodos)
+
+    $('#todoInput').keypress(e => {
+        if (e.which == 13) {
+            createTodo();
+        }
+    })
 });
+
+function createTodo() {
+    // send request to create todo
+    let usrInput = $('#todoInput').val();
+    $.post('/api/todos', { name: usrInput })
+    .then(newTodo => {
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+
+function addTodo(todo) {
+    const newTodo = $(`<li class="task">${todo.name}</li>`);
+    if (todo.completed) {
+        newTodo.addClass('done');
+    }
+    $('.list').append(newTodo);
+}
 
 function addTodos(todos) {
     // add todos to page
     todos.forEach(todo => {
-        const newTodo = $(`<li class="task">${todo.name}</li>`);
-        if (todo.completed) {
-            newTodo.addClass('done');
-        }
-        $('.list').append(newTodo);
+        addTodo(todo);
     });
 }
