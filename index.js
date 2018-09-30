846982eb6d73c59a4dd7b7eec415e9f5207ccf7b
lucasflomuller <lucasflorianomuller@gmail.com>
2018-09-30 15:42:54 +0000

    initial configs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+      app = express(),
+      PORT = process.env.PORT || '8080',
+      IP = process.env.IP || '0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.send("HI THERE")
+});
+
+app.listen(PORT, IP, () => {
+  console.log(`APP IS RUNNING ON PORT: ${PORT}`);
+})
