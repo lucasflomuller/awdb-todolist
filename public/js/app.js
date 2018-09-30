faf425ad0cc8a1b999d48c349f74d145ae9ba68f
lucasflomuller <lucasflorianomuller@gmail.com>
2018-09-30 18:50:08 +0000

    display completed todos

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,9 @@ function addTodos(todos) {
     // add todos to page
     todos.forEach(todo => {
         const newTodo = $(`<li class="task">${todo.name}</li>`);
+        if (todo.completed) {
+            newTodo.addClass('done');
+        }
         $('.list').append(newTodo);
     });
 }
