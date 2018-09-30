560f822b4fa6d53a853d53d4896c60f22cdbd693
lucasflomuller <lucasflorianomuller@gmail.com>
2018-09-30 16:06:28 +0000

    define index route

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require('express'),
       PORT = process.env.PORT || '8080',
       IP = process.env.IP || '0.0.0.0';
 
+const todoRoutes = require('./routes/todos');
+
 app.get('/', (req, res) => {
-  res.send("HI THERE")
+  res.send("HI THERE");
 });
+
+app.use('/api/todos', todoRoutes);
 
 app.listen(PORT, IP, () => {
   console.log(`APP IS RUNNING ON PORT: ${PORT}`);
